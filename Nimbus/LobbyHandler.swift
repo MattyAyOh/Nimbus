a44aaf978504007ba76e3fb3a4c137cf43d6d265
Michael Anderson <anderson.mi91@gmail.com>
2017-02-01 18:21:53 -0500

    Initial commit

---
+++ Nimbus/LobbyHandler.swift
@@ -0,0 +1,21 @@
+//
+//  LobbyHandler.swift
+//  Nimbus
+//
+//  Created by Michael Anderson on 1/31/17.
+//  Copyright © 2017 Michael Anderson. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+class LobbyHandler {
+    
+    var subscriptionIslocallyCached = false
+    
+    func loginToICloud() {
+        if (subscriptionIslocallyCached) {return}
+        
+
+    }
+}
